e5a73bd6f850090d3469b4cb28ceb54aadeee070
Not-Igor <95211298+Not-Igor@users.noreply.github.com>
2025-12-03 18:13:51 +0100

    test

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -52,7 +52,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Wachtwoord"
+                placeholder="Wachtwoorddddd"
               />
             </div>
           </div>
